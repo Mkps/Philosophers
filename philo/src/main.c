d7de48959c6d8befd19dc0b05231ef2960093106
Alexis LOUBIERE <alex.loubiere@gmail.com>
2023-07-27 13:14:53 +0200

    working timestamp

--- philo/src/main.c
+++ philo/src/main.c
@@ -1,50 +1,49 @@
 #include "../includes/philo.h"
 
 typedef struct timeval tv;
-void	timestamp(long tc, tv tval)
+void	timestamp(tv start)
 {
-	gettimeofday(&tval, NULL);
-	printf("[ms : %ld]   ", tval.tv_usec - tc);
+	tv	end;
+
+	gettimeofday(&end, NULL);
+	printf("[ms : %ld ", ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000 );
 }
+
+typedef struct philosopher {
+	int	is_alive;
+	long	tts;
+	long	tte;
+	long	ttd;
+} t_philo;
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
-	long	tc;
-	tv value;
+	tv		tc;
+	tv		to;
+	tv		cycle;
+	t_philo phi;
 
-	gettimeofday(&value, NULL);
-	tc = value.tv_usec;
-	pid_t f = fork();
-	if (!f)
+	if (argc != 5)
+		return (0);
+	gettimeofday(&to, NULL);
+	phi.is_alive = 1;
+	phi.ttd = atol(argv[2]);
+	phi.tte = atoi(argv[3]);
+	phi.tts = atol(argv[4]);
+	while (phi.is_alive)
 	{
-		timestamp(tc, value);
-		printf("child\n");	
+		gettimeofday(&cycle, NULL);
+		timestamp(to);	
+		printf("phi is eating\n");
+		usleep(phi.tte * 1000);
+		timestamp(to);	
+		printf("phi is sleeping\n");
+		usleep(phi.tts * 1000);
+		gettimeofday(&tc, NULL);
+		if (((tc.tv_sec * 1000000 + tc.tv_usec) - (cycle.tv_sec * 1000000 + cycle.tv_usec)) / 1000 > phi.ttd)
+			phi.is_alive = 0;
 	}
-	else
-	{
-		wait(NULL);
-		timestamp(tc, value);
-		printf("parent\n");
-	
-	pid_t p = fork();
-	if (!p)
-	{
-		for (int i=0; i < 500; i++)
-		{
-			if (i == 0)
-			{
-				timestamp(tc, value);
-				printf("waiting...\n");
-			}
-		}
-	}
-	else
-	{
-		wait(NULL);
-		timestamp(tc, value);
-		printf("DONE\n");
-	}
-	}
+	timestamp(to);	
+	printf("phi died...\n");
 	return (0);
 }
